19c4cdf1d657dd797ebe6cbbcef6223faa87af31
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-25 17:22:26 +0100

    Called APIs

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -13,7 +13,6 @@ const data:Data ={
     domain: "sanantonioherniaspecialists.com",
     logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719124349/SHS_gmm7mj.png',
     src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d444671.3114320745!2d-98.84402845743045!3d29.45816741495709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x865c58af04d00eaf%3A0x856e13b10a016bc!2sSan%20Antonio%2C%20TX%2C%20USA!5e0!3m2!1sen!2sng!4v1719124200902!5m2!1sen!2sng"
-
 }
 
 export default data
